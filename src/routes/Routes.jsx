fd10ae1f20354172fb747b4ae8d84a9f175da9de
ISHRAK-Wants-Burger <islamminhajul60@gmail.com>
2023-04-30 23:40:29 +0600

    hosted to firebase

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-server-ishrak-wants-burger.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-server-ishrak-wants-burger.vercel.app/news/${params.id}`)
             }
         ]
     }
